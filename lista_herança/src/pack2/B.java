a2bbe59d940aba7d592b1ddb9cd57a34f391c8f7
Luan <luan@plannerti.com.br>
2021-05-17 12:38:06 -0300

    lista herança

---
+++ lista_herança/src/pack2/B.java
@@ -0,0 +1,13 @@
+package pack2;
+
+import pack1.A;
+
+class B extends A {
+	int methodOne(int i) {
+		return methodTwo(++i);
+	}
+}
+
+
+
+
---
+++ lista_herança/src/pack2/MainClass.java
@@ -0,0 +1,9 @@
+package pack2;
+import pack1.A;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println(new B().methodOne(101));
+	}
+}
